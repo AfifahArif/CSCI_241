e9642aafadcd6392d61d611f4713ecab49c4bc68
Afifah Arif <cstudy07@gmail.com>
2019-01-15 21:45:29 -0600

    Update program1.cpp

--- Program1/program1.cpp
+++ Program1/program1.cpp
@@ -29,54 +29,54 @@ int main()
 	// Tests 1a - 1d: Test the equals() function
 	cout << "Test 1a: ";
 	if (equals("catapult", "catapult"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << "Test 1b: ";
 	if (!equals("catapult", "catamaran"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << "Test 1c: ";
 	if (!equals("cat", "catamaran"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << "Test 1d: ";
 	if (!equals("catapult", "cat"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << endl;
 
 	// Tests 2a - 2d: Test the equalsIgnoreCase() function
 	cout << "Test 2a: ";
 	if (equalsIgnoreCase("catapult", "CATAPULT"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << "Test 2b: ";
 	if (!equalsIgnoreCase("catapult", "Catamaran"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << "Test 2c: ";
 	if (!equalsIgnoreCase("Cat", "Catamaran"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << "Test 2d: ";
 	if (!equalsIgnoreCase("Catapult", "Cat"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << endl;
 
@@ -84,30 +84,30 @@ int main()
 	cout << "Test 3a: ";
 	index = indexOf("adamant", 'a');
 	if (index == 0)
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect - index returned was " << index << ", not 0\n";
+		cout << "incorrect - index returned was " << index << ", not 0\n";
 
 	cout << "Test 3b: ";
 	index = indexOf("adamant", 'm');
 	if (index == 3)
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect - index returned was " << index << ", not 3\n";
+		cout << "incorrect - index returned was " << index << ", not 3\n";
 
 	cout << "Test 3c: ";
 	index = indexOf("adamant", 't');
 	if (index == 6)
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect - index returned was " << index << ", not 6\n";
+		cout << "incorrect - index returned was " << index << ", not 6\n";
 
 	cout << "Test 3d: ";
 	index = indexOf("adamant", 'x');
 	if (index == -1)
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect - index returned was " << index << ", not -1\n";
+		cout << "incorrect - index returned was " << index << ", not -1\n";
 
 	cout << endl;
 
@@ -115,72 +115,72 @@ int main()
 	cout << "Test 4a: ";
 	index = lastIndexOf("adamant", 't');
 	if (index == 6)
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect - index returned was " << index << ", not 6\n";
+		cout << "incorrect - index returned was " << index << ", not 6\n";
 
 	cout << "Test 4b: ";
 	index = lastIndexOf("adamant", 'm');
 	if (index == 3)
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect - index returned was " << index << ", not 3\n";
+		cout << "incorrect - index returned was " << index << ", not 3\n";
 
 	cout << "Test 4c: ";
 	index = lastIndexOf("adamant", 'a');
 	if (index == 4)
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect - index returned was " << index << ", not 4\n";
+		cout << "incorrect - index returned was " << index << ", not 4\n";
 
 	cout << "Test 4d: ";
 	index = lastIndexOf("adamant", 'x');
 	if (index == -1)
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect - index returned was " << index << ", not -1\n";
+		cout << "incorrect - index returned was " << index << ", not -1\n";
 
 	cout << endl;
 
 	// Tests 5a - 5c: Test the startsWith() function
 	cout << "Test 5a: ";
 	if (startsWith("your house", "your"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << "Test 5b: ";
 	if (!startsWith("my house", "your"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << "Test 5c: ";
 	if (!startsWith("you", "your"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << endl;
 
 	// Tests 6a - 6c: Test the endsWith() function
 	cout << "Test 6a: ";
 	if (endsWith("your house", "house"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << "Test 6b: ";
 	if (!endsWith("your mouse", "house"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	cout << "Test 6c: ";
 	if (!endsWith("use", "house"))
-	cout << "correct\n";
+		cout << "correct\n";
 	else
-	cout << "incorrect\n";
+		cout << "incorrect\n";
 
 	return 0;
 }
